b407aaacde79c867a4ce6a6a174d9d0fab487d0a
arisapooeater <arisa.komatsu@education.nsw.gov.au>
2025-11-17 20:36:12 +1100

    3/4 way through tutorial 3

--- Assets/Scripts/Ball.cs
+++ Assets/Scripts/Ball.cs
@@ -1,6 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
 using UnityEngine;
 
-public class Ball1
+public class Ball : MonoBehaviour
 {
-    
+    private Rigidbody _rb;
+    [SerializeField]
+    private Vector3 _startingPosition;
+
+    void Start()
+    {
+        _rb = GetComponent<Rigidbody>();
+        if (_rb == null)
+        {
+            Debug.Log("Rigidbody is NULL");
+        }
+    }
+
+    void Update()
+    {
+        CheckForOutOfBounds();
+    }
+
+    void CheckForOutOfBounds()
+    {
+        if (transform.position.y < -20)
+        {
+            _rb.linearVelocity = Vector3.zero;
+            transform.position = _startingPosition;
+        }
+    }
 }
--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -15,6 +15,7 @@ public class Player : MonoBehaviour
     private float _jump = 5.0f;
     [SerializeField]
     private Vector3 _startingPosition;
+    private Vector3 dir;
 
     [Header("Camera Settings")]
     [SerializeField]
@@ -22,12 +23,45 @@ public class Player : MonoBehaviour
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        _controller = GetComponent<CharacterController>();
+        if(_controller == null){
+            Debug.Log("Controller is NULL");
+        }
+        _mainCam = Camera.main;
+        if(_mainCam == null){
+            Debug.Log("Main Camera is NULL");
+        }
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        Move();
+        CheckForOutOfBounds();
+    }
+
+    void CheckForOutOfBounds()
+    {
+
+    }
+    void Move()
+    {
+        if (_controller.isGrounded == true)
+        {
+            float horizontalInput = Input.GetAxis("Horizontal");
+            float verticalInput = Input.GetAxis("Vertical");
+
+            dir = new Vector3(verticalInput, 0, -horizontalInput);
+            dir = transform.TransformDirection(dir);  
+
+            if (Input.GetKeyDown(KeyCode.Space))
+            {
+                dir.y = _jump;
+            }
+        }
+
+        dir.y -= _gravity * Time.deltaTime;
+        _controller.Move(dir * _speed * Time.deltaTime);
     }
 }
